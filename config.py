71df81dc35cba90677358fd1948494906f0caa35
Lucas Silva <57262360+AntonioAzevedoDev@users.noreply.github.com>
2024-06-05 16:34:14 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+EMAIL_ADDRESS = 'chamado@evolutionit.com.br'
+EMAIL_PASSWORD = 'agua9999'
+SMTP_SERVER = 'smtp.kinghost.net'
+SMTP_PORT = 587
+DOMAIN = 'evolutionit.com.br'
+AUTH_METHOD = 'login'
+ENABLE_STARTTLS_AUTO = True
+OPENSSL_VERIFY_MODE = 'none'
+
+
+REDMINE_URL = 'https://redmine5tec.evtit.com'
+REDMINE_API_KEY = "ea8d896c01e60cbc31baf6e84e9d4bf8eee5033b"
+#REDMINE_URL = os.getenv('REDMINE_URL')
+#REDMINE_API_KEY = os.getenv('REDMINE_API_KEY')
